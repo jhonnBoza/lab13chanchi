5b08bde98d30aca97197edb669fd62cab59bee55
Boza <168006484+jhonnBoza@users.noreply.github.com>
2025-11-12 17:10:07 -0500

    xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Hero from './components/Hero'
 import Entities from './components/Entities'
 
 function App() {
+  const [flags, setFlags] = useState([])
+
   useEffect(() => {
     const fetchCountries = async () => {
       const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags')
       const data = await response.json()
       console.log(data)
+      setFlags(data)
     }
     fetchCountries()
   }, [])
@@ -15,7 +18,7 @@ function App() {
   return (
     <>
       <Hero />
-      <Entities />
+      <Entities data={flags} />
     </>
   )
 }
